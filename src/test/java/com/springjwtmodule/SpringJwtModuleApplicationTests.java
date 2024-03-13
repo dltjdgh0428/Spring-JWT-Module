8e84c39865269b098adf1c5efb9113f2c3eb2755
dltjdgh0428 <dltjdgh0428@naver.com>
2024-03-14 05:04:49 +0900

    Build : JWT 모듈 코딩 시작!!

---
+++ src/test/java/com/springjwtmodule/SpringJwtModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springjwtmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJwtModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
